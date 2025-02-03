16c52fd287113c0b0b24369dc3fd79ec85a94230
Arsh2300 <arshdeep0181.becse24@chitkara.edu.in>
2025-02-03 21:08:16 +0530

    Solution to Check Greater Than

---
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    int b;
+    scanf("%d%d,&a,&b");
+    if a>b
+    {
+        printf("Trur");
+
+    }
+    else
+    {
+        printf("False");
+    }
+    
+    return 0;
+}
