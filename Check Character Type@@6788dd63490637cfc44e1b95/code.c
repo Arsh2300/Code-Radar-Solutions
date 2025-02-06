2337cafb9643caf69c2b601b0030f1833a0daa33
Arsh2300 <arshdeep0181.becse24@chitkara.edu.in>
2025-02-06 12:18:59 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -4,20 +4,24 @@
 //     return "Welcome to Code Radar!";
 // }
 
-// int main() {
+int main() {
 //     printf("%s", welcome());
 //     return 0;
 // }
-int main() {
-    char num;
-    scanf("%c",&num);
-    
-    if (num=='a'||num=='e'||num=='i'||num=='o'||num=='u'){
-        printf("Vowel");
+char ch;
+printf("",ch);
+scanf("%c",ch);
+if(ch=='A' || ch=='E'||ch=='I' || ch=='O'||ch=='U' || ch=='a'||ch=='e' || ch=='i'||ch=='o' || ch=='u'||){
+    printf("Vowel");
+}
+else if(ch>='A'&& ch<='Z')||(ch>='a'&& ch<='z'){
+    printf("Consonant");
 
-    }else{
-        printf("Consonant");
-    }
-            return 0;
-                } 
-
+}
+else if (ch>=0 && ch<=9){
+    printf("Digit");
+}
+else{
+    printf("Special Character");
+}
+}
